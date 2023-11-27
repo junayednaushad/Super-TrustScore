64969174c01f41ead1dc3ebe3e6b5a3112fb273a
Junayed Naushad <sann7666@htc-g040.arcus-htc.arc.local>
2023-11-27 23:47:28 +0000

    added preprocessing for Messidor-2

--- src/preprocessing/utils.py
+++ src/preprocessing/utils.py
@@ -81,3 +81,76 @@ def apply_cc(img_paths, output_folder_path, resize=False):
         cv2.imwrite(
             os.path.join(output_folder_path, img_name.split(".")[0] + ".jpg"), np_img
         )
+
+
+def btgraham_processing(filepath, extract_dir, target_pixels=300, crop_to_radius=True):
+    """
+    Source:
+    https://www.kaggle.com/code/ratthachat/aptos-eye-preprocessing-in-diabetic-retinopathy?scriptVersionId=20340219
+
+    Process an image as the winner of the 2015 Kaggle competition.
+
+    Args:
+      image_fobj: File object containing the original image.
+      filepath: Filepath of the image, for logging purposes only.
+      target_pixels: The number of target pixels for the radius of the image.
+      crop_to_radius: If True, crop the borders of the image to remove gray areas.
+    Returns:
+      A file object.
+
+    """
+    # Decode image using OpenCV2.
+    image = cv2.imread(str(filepath), flags=3)
+    # Process the image.
+    image = scale_radius_size(image, filepath, target_radius_size=target_pixels)
+    image = subtract_local_average(image, target_radius_size=target_pixels)
+    image = mask_and_crop_to_radius(
+        image,
+        target_radius_size=target_pixels,
+        radius_mask_ratio=0.9,
+        crop_to_radius=crop_to_radius,
+    )
+
+    # Encode the image with quality=72 and store it to disk
+    cv2.imwrite(
+        os.path.join(extract_dir, filepath.split("/")[-1]),
+        image,
+        [int(cv2.IMWRITE_JPEG_QUALITY), 72],
+    )
+
+
+def scale_radius_size(image, filepath, target_radius_size):
+    """Scale the input image so that the radius of the eyeball is the given."""
+    x = image[image.shape[0] // 2, :, :].sum(axis=1)
+    r = (x > x.mean() / 10.0).sum() / 2.0
+    if r < 1.0:
+        # Some images in the dataset are corrupted, causing the radius heuristic to
+        # fail. In these cases, just assume that the radius is the height of the
+        # original image.
+        r = image.shape[0] / 2.0
+    s = target_radius_size / r
+    return cv2.resize(image, dsize=None, fx=s, fy=s)
+
+
+def subtract_local_average(image, target_radius_size):
+    image_blurred = cv2.GaussianBlur(image, (0, 0), target_radius_size / 30)
+    image = cv2.addWeighted(image, 4, image_blurred, -4, 128)
+    return image
+
+
+def mask_and_crop_to_radius(
+    image, target_radius_size, radius_mask_ratio=0.9, crop_to_radius=False
+):
+    """Mask and crop image to the given radius ratio."""
+    mask = np.zeros(image.shape)
+    center = (image.shape[1] // 2, image.shape[0] // 2)
+    radius = int(target_radius_size * radius_mask_ratio)
+    cv2.circle(mask, center=center, radius=radius, color=(1, 1, 1), thickness=-1)
+    image = image * mask + (1 - mask) * 128
+    if crop_to_radius:
+        x_max = min(image.shape[1] // 2 + radius, image.shape[1])
+        x_min = max(image.shape[1] // 2 - radius, 0)
+        y_max = min(image.shape[0] // 2 + radius, image.shape[0])
+        y_min = max(image.shape[0] // 2 - radius, 0)
+        image = image[y_min:y_max, x_min:x_max, :]
+    return image
